74f32bdb217864461e9b214bcc33ae9785bc7e08
arash amirsharifi <arashamirsharifi1970@gmail.com>
2024-01-27 21:20:14 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="flex flex-col gap-2 mt-3">
+      <h1 className="text-blue-500 text-2xl">love faeze baby 😜🎈</h1>
+      <p> a love yoy so mush you are my life </p>
+    </div>
+  );
+}
