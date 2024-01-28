ed7def43b81beeb42fcc4062413e89f85160b503
arash amirsharifi <arashamirsharifi1970@gmail.com>
2024-01-28 23:21:37 +0330

    fixerror

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -6,21 +6,21 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaPhoneAlt } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { LiaAddressCardSolid } from "react-icons/lia";
+import pic2 from "../../../public/images/1.jpg";
+import pic3 from "../../../public/images/2.jpg";
 export default function AboutPage() {
   const [aboutPreson, setAboutPreson] = useState(aboutPresonData[0]);
-  console.log(aboutPreson);
+  console.log(aboutPreson.img);
+  const pic =
+    "w-[70%] bg-red-200 md:w-[75%] h-[30%] md:h-[75%] rounded-lg shadow-lg border-4";
   return (
     <div className=" mt-12 flex flex-col md:flex-row     justify-around p-5">
-      <div className="w-[80%] md:w-[30%] mx-auto md:mx-0  flex items-center justify-center ">
-        <div className=" w-[50%] md:w-[75%] h-[30%] md:h-[75%] rounded-lg shadow-lg border-4">
-          <img
-            className="w-full h-full rounded-sm"
-            src={aboutPreson.img}
-            alt="noote"
-          />
+      <div className="w-[80%]  md:w-[30%] mx-auto md:mx-0  flex items-center justify-center ">
+        <div className={pic}>
+          <img className="w-full h-full" src={pic2} alt="nooot" />
         </div>
       </div>
-      <div className="flex flex-col mx-auto md:mx-0 justify-center gap-5 p-4 m-4 rounded-lg shadow-2xl shadow-gray-600 w-[100%] md:w-[60%]  bg-ferveritPink text-white pl-8 font-comfortaa">
+      <div className="flex flex-col mx-auto md:mx-0 justify-center gap-5 p-4 m-4 rounded-lg shadow-2xl shadow-gray-600 w-[90%] md:w-[60%]  bg-ferveritPink text-white pl-8 font-comfortaa">
         <h1 className="mb-10">HELLOW I AM </h1>
         <h2 className="text-4xl font-bold">{aboutPreson.name}</h2>
         <h3 className="text-3xl">{aboutPreson.lastname}</h3>
@@ -48,7 +48,7 @@ export default function AboutPage() {
             <MdEmail />
             <p>gmail:</p>
           </div>
-          <p>{aboutPreson.gmail}</p>
+          <p className="text-[13px] md:text-md">{aboutPreson.gmail}</p>
         </div>
         <div className="flex gap-3 p-1 items-center">
           <div className="flex gap-2  items-center ">
